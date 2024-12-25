fb96a86d62974383cce19ad9d0a5015d81478111
Yash <yashb967@gmail.com>
2024-12-25 20:44:57 +0530

    Data Ingestion

---
+++ app.py
@@ -0,0 +1,23 @@
+from source.MLproject.logger import logging
+from source.MLproject.exception import CustomException
+from source.MLproject.components.data_ingestion import DataIngestion
+from source.MLproject.components.data_ingestion import DataIngestionConfig
+import sys 
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+
+
+
+    except Exception as e:
+
+        logging.info("Custom Exception")
+        
+        raise CustomException(e,sys)
+
+
---
+++ source/MLproject/utils.py
@@ -0,0 +1,37 @@
+import os
+import sys 
+from source.MLproject.logger import logging 
+from source.MLproject.exception import CustomException
+import pandas as pd 
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv('db')
+
+
+def read_sql_data():
+    logging.info('Reading SQL database has started')
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db='college'
+
+        )
+        logging.info("Connectio established",mydb)
+        df=pd.read_sql_query('Select * from student',mydb)
+        print(df.head())
+
+
+        return df 
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
